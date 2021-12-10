85c814820886d4aaf64fc7e89c20833f162562e6
Guilherme Veiga <guilherme.dsveiga@gmail.com>
2021-12-10 16:59:55 -0300

    Finalizando a página principal

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-	<footer id="colophon" class="mt-5 py-4 site-footer bg-black text-white">
+	<footer id="colophon" class="py-4 site-footer bg-black text-white">
 		<div class="d-flex px-5 justify-content-between">
 			<div class="me-5">
 				<img class="w-100" src="<?php echo get_bloginfo('template_url') ?>/assets/orion-logo.png" alt="Logo da empresa Orion Maritima">
@@ -6,23 +6,23 @@
 			<div class="infos d-flex">
 				<div class="d-flex flex-column px-4">
 					<h3 class="fw-bold mb-4">Acesso Rápido</h3>
-					<a href=""> > <span>Home</span></a>
-					<a href=""> > <span>Quem Somos</span></a>
+					<a class="text-decoration-none text-white" href=""> > <span>Home</span></a>
+					<a class="text-decoration-none text-white" href=""> > <span>Quem Somos</span></a>
 				</div>
 				<div class="d-flex flex-column px-4">
 					<h3 class="fw-bold mb-4"">Contato</h3>
 				<p class=" mb-4">Devido a pandemia do COVID-19, neste momento, por gentileza nos contate através do nosso e-mail geral:</p>
 						<div class="email d-flex">
-							<img src="<?php echo get_bloginfo('template_url') ?>" alt="Icone de e-mail">
+							<img class="max-icon me-2" src="<?php echo get_bloginfo('template_url') ?>/assets/mail.svg" alt="Icone de e-mail">
 							<p>bpwr@amorion.com.br</p>
 						</div>
 				</div>
 				<div class="d-flex flex-column px-4">
 					<h3 class="fw-bold mb-4"">Redes Sociais</h3>
 				<div class=" d-flex">
-						<img src="<?php echo get_bloginfo('template_url') ?>" alt="Icone do Facebook">
-						<img src="<?php echo get_bloginfo('template_url') ?>" alt="Icone do Linkedin">
-						<img src="<?php echo get_bloginfo('template_url') ?>" alt="Icone do Instagram">
+						<img class="max-icon me-2 bg-white" src="<?php echo get_bloginfo('template_url') ?>/assets/facebook.svg" alt="Icone do Facebook">
+						<img class="max-icon me-2 bg-white" src="<?php echo get_bloginfo('template_url') ?>/assets/linkedin.svg" alt="Icone do Linkedin">
+						<img class="max-icon me-2 bg-white" src="<?php echo get_bloginfo('template_url') ?>/assets/instagram.svg" alt="Icone do Instagram">
 				</div>
 			</div>
 		</div>
