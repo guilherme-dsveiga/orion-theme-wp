c7a1460d7e78c80f68b9fa4b495aedb90add7833
guilherme-dsveiga <guilherme.dsveiga@gmail.com>
2021-12-17 13:15:09 -0300

    Finalizando pagina de serviços

---
+++ services-page.php
@@ -0,0 +1,61 @@
+<!--?php /* Template name: Services Page */ ?-->
+<?
+get_header();
+?>
+
+<main id="primary" class="site-main">
+	<section class="sec1 d-flex flex-column justify-content-center p-sec">
+		<div class="cont-max">
+			<h5 class="p-2 bg-black fit-content text-white rounded mb-3">Agenciamento Marítimo</h5>
+			<div class="w-100 d-flex justify-content-between">
+				<h1 class="text-white font-huge mb-4"><span class="fw-bold">Inteligencia aplicada</span><br> em <span class="fw-bold">todas as escalas</span></h1>
+			</div>
+			<p class="text-white pe-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis sapiente consequuntur, eos dolorem molestias atque laboriosam velit vitae aut vel tempore esse ad nulla ipsam architecto aspernatur? Soluta, placeat totam? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, praesentium, eos delectus magni in non inventore commodi, officia corporis ullam accusantium consequuntur. Voluptates illo eius dolores? In illo voluptas vel? Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+			<p class="text-white mb-4 pe-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, corrupti aut dolorem iste illum consequuntur minima reprehenderit ratione iusto repudiandae, rem inventore est! Ipsa dolores dolor perferendis beatae quaerat error? Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+			<button type="button" class="btn  btn-primary fs-5">entre em contato</button>
+		</div>
+	</section>
+	<section class="sec2 d-flex flex-column justify-content-center align-items-end p-sec">
+		<div class="cont-max text-end d-flex flex-column align-items-end">
+			<h5 class="p-2 bg-black fit-content text-white rounded mb-3">Agenciamento Marítimo</h5>
+			<h1 class="text-white font-huge mb-4"><span class="fw-bold">Inteligencia aplicada</span><br> em <span class="fw-bold">todas as escalas</span></h1>
+			<p class="text-white ps-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis sapiente consequuntur, eos dolorem molestias atque laboriosam velit vitae aut vel tempore esse ad nulla ipsam architecto aspernatur? Soluta, placeat totam? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, praesentium, eos delectus magni in non inventore commodi, officia corporis ullam accusantium consequuntur. Voluptates illo eius dolores? In illo voluptas vel? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui voluptatibus suscipit molestiae asperiores nesciunt quo ipsam perferendis laborum, natus recusandae adipisci porro quae, minus, culpa ipsa quia tenetur vitae ad.</p>
+			<p class="text-white mb-4 ps-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, corrupti aut dolorem iste illum consequuntur minima reprehenderit ratione iusto repudiandae, rem inventore est! Ipsa dolores dolor perferendis beatae quaerat error? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni rerum veritatis quas voluptate nobis ea earum doloremque repellat, qui facilis voluptas rem voluptatibus placeat.</p>
+			<button type="button" class="btn ms-auto btn-primary fs-5">entre em contato</button>
+		</div>
+	</section>
+	<section class="sec3 d-flex flex-column justify-content-center p-sec">
+		<div class="cont-max">
+			<h5 class="p-2 bg-black fit-content text-white rounded mb-3">Agenciamento Marítimo</h5>
+			<div class="w-100 d-flex justify-content-between">
+				<h1 class="text-white font-huge mb-4"><span class="fw-bold">Inteligencia aplicada</span><br> em <span class="fw-bold">todas as escalas</span></h1>
+			</div>
+			<p class="text-white pe-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis sapiente consequuntur, eos dolorem molestias atque laboriosam velit vitae aut vel tempore esse ad nulla ipsam architecto aspernatur? Soluta, placeat totam? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, praesentium, eos delectus magni in non inventore commodi, officia corporis ullam accusantium consequuntur. Voluptates illo eius dolores? In illo voluptas vel? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui voluptatibus suscipit molestiae asperiores nesciunt quo ipsam perferendis laborum, natus recusandae adipisci porro quae, minus, culpa ipsa quia tenetur vitae ad.</p>
+			<p class="text-white mb-4 pe-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, corrupti aut dolorem iste illum consequuntur minima reprehenderit ratione iusto repudiandae, rem inventore est! Ipsa dolores dolor perferendis beatae quaerat error? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni rerum veritatis quas voluptate nobis ea earum doloremque repellat, qui facilis voluptas rem voluptatibus placeat.</p>
+			<button type="button" class="btn btn-primary fs-5">entre em contato</button>
+		</div>
+	</section>
+	<section class="sec4 d-flex flex-column justify-content-center align-items-end p-sec">
+		<div class="cont-max text-end d-flex flex-column align-items-end">
+			<h5 class="p-2 bg-black fit-content text-white rounded mb-3">Agenciamento Marítimo</h5>
+			<h1 class="text-white font-huge mb-4"><span class="fw-bold">Inteligencia aplicada</span><br> em <span class="fw-bold">todas as escalas</span></h1>
+			<p class="text-white ps-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis sapiente consequuntur, eos dolorem molestias atque laboriosam velit vitae aut vel tempore esse ad nulla ipsam architecto aspernatur? Soluta, placeat totam? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, praesentium, eos delectus magni in non inventore commodi, officia corporis ullam accusantium consequuntur. Voluptates illo eius dolores? In illo voluptas vel? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui voluptatibus suscipit molestiae asperiores nesciunt quo ipsam perferendis laborum, natus recusandae adipisci porro quae, minus, culpa ipsa quia tenetur vitae ad.</p>
+			<p class="text-white mb-4 ps-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, corrupti aut dolorem iste illum consequuntur minima reprehenderit ratione iusto repudiandae, rem inventore est! Ipsa dolores dolor perferendis beatae quaerat error? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni rerum veritatis quas voluptate nobis ea earum doloremque repellat, qui facilis voluptas rem voluptatibus placeat.</p>
+			<button type="button" class="btn ms-auto btn-primary fs-5">entre em contato</button>
+		</div>
+	</section>
+	<section class="sec5 d-flex flex-column justify-content-center p-sec">
+		<div class="cont-max">
+			<h5 class="p-2 bg-black fit-content text-white rounded mb-3">Agenciamento Marítimo</h5>
+			<div class="w-100 d-flex justify-content-between">
+				<h1 class="text-white font-huge mb-4"><span class="fw-bold">Inteligencia aplicada</span><br> em <span class="fw-bold">todas as escalas</span></h1>
+			</div>
+			<p class="text-white pe-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis sapiente consequuntur, eos dolorem molestias atque laboriosam velit vitae aut vel tempore esse ad nulla ipsam architecto aspernatur? Soluta, placeat totam? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, praesentium, eos delectus magni in non inventore commodi, officia corporis ullam accusantium consequuntur. Voluptates illo eius dolores? In illo voluptas vel? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui voluptatibus suscipit molestiae asperiores nesciunt quo ipsam perferendis laborum, natus recusandae adipisci porro quae, minus, culpa ipsa quia tenetur vitae ad.</p>
+			<p class="text-white mb-4 pe-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, corrupti aut dolorem iste illum consequuntur minima reprehenderit ratione iusto repudiandae, rem inventore est! Ipsa dolores dolor perferendis beatae quaerat error? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni rerum veritatis quas voluptate nobis ea earum doloremque repellat, qui facilis voluptas rem voluptatibus placeat.</p>
+			<button type="button" class="btn btn-primary fs-5">entre em contato</button>
+		</div>
+	</section>
+</main><!-- #main -->
+
+<?php
+get_footer();
